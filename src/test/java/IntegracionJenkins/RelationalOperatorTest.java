e3d13b85fbcbf21a8bf1833f9388da49da22a54c
lasarang <lasarang@espol.edu.ec>
2022-07-22 16:47:56 -0500

    Adding isGreaterTest3 and isLessTest3

--- src/test/java/IntegracionJenkins/RelationalOperatorTest.java
+++ src/test/java/IntegracionJenkins/RelationalOperatorTest.java
@@ -17,16 +17,28 @@ public class RelationalOperatorTest {
 		RelationalOperator tester = new RelationalOperator();
 		assertTrue(tester.isGreater(2, 1));
 	}
+	
+	@Test
+	public void isGreaterTest3() {
+		RelationalOperator tester = new RelationalOperator();
+		assertFalse(tester.isGreater(3, 3));
+	}
 
 	@Test
 	public void isLessTest1() {
 		RelationalOperator tester = new RelationalOperator();
-		assertTrue(tester.isLess(4, 4));
+		assertTrue(tester.isLess(3, 4));
 	}
 	
 	@Test
 	public void isLessTest2() {
 		RelationalOperator tester = new RelationalOperator();
 		assertFalse(tester.isLess(5, 1));
+	}
+	
+	@Test
+	public void isLessTest3() {
+		RelationalOperator tester = new RelationalOperator();
+		assertFalse(tester.isLess(5, 5));
 	}
 }
